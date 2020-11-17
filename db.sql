3f3c1c46baea2e09865f320020fc93a79e2a0060
hahyeon1132 <kogw66@gmail.com>
2020-11-17 10:52:21 +0900

    connect database

---
+++ db.sql
@@ -0,0 +1,27 @@
+SELECT * FROM Java.Article;
+
+insert into article(
+set regDate=now(),
+updateDate = now(),
+title ='제목1',
+body = '제목1',
+memberId = 1,
+boardId = 1);
+
+
+insert into article(
+set regDate=now(),
+updateDate = now(),
+title ='제목2',
+body = '제목2',
+memberId = 2,
+boardId = 2);
+
+
+insert into article(
+set regDate=now(),
+updateDate = now(),
+title ='제목3',
+body = '제목3',
+memberId = 3,
+boardId = 3);
--- src/Dao/ArticleDao.java
+++ src/Dao/ArticleDao.java
@@ -1,31 +1,48 @@
 package Dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import Dto.Article;
 
 public class ArticleDao {
-	List<Article> articles;
-	int lastID;
-	public ArticleDao() {
-		articles = new ArrayList<>();
-		lastID = 0;
-	
-	}
+
 	public List<Article> showArticleList() {
+		List<Article> articles = new ArrayList<>();
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/Java?serverTimezone=Asia/Seoul", "sbsst",
+					"sbs123414");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+		return articles;
+	}
+
+	private List<Article> getAritlces() {
+		List<Article> articles = new ArrayList<>();
 		Article article = new Article();
-		
-		article.id = lastID + 1;
+
+		article.id = 1;
 		article.title = "제목";
 		article.body = "내용";
 		article.memberid = 1;
 		article.rageDate = "2020-11-17";
-		article.updateDate= "2020-11-17";
-		
+		article.updateDate = "2020-11-17";
+
 		articles.add(article);
-		lastID = article.id;
-		
+
 		return articles;
 	}
 
