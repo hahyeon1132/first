94414f53ab40e108ac253b52648b3516e6a144df
hahyeon1132 <kogw66@gmail.com>
2020-11-17 11:59:24 +0900

    최신순으로 개시물 불러오기

--- src/Dao/ArticleDao.java
+++ src/Dao/ArticleDao.java
@@ -23,27 +23,27 @@ public class ArticleDao {
 		}
 		try {
 
-			String sql = "SELECT * FROM Article LIMIT 1";
+			String sql = "SELECT * FROM Article ORDER BY id DESC";
 			PreparedStatement pstmt = con.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
-			rs.next();
-			Article article = new Article();
-			int id = rs.getInt("id");
-			String title = rs.getString("title");
-			String body = rs.getString("body");
-			String updatedate = rs.getString("updatedate");
-			String regdate = rs.getString("regdate");
-			int memberid = rs.getInt("memberid");
-			int boardid = rs.getInt("boardid");
-			
-			article.id = id;
-			article.title = title;
-			article.body = body;
-			article.updateDate = updatedate;
-			article.rageDate = regdate;
-			article.memberid = memberid;
-			article.boardid = boardid;
-			articles.add(article);
+			while(rs.next()) {
+				Article article = new Article();
+				int id = rs.getInt("id");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
+				String updatedate = rs.getString("updatedate");
+				String regdate = rs.getString("regdate");
+				int memberid = rs.getInt("memberid");
+				int boardid = rs.getInt("boardid");
+				article.id = id;
+				article.title = title;
+				article.body = body;
+				article.updateDate = updatedate;
+				article.rageDate = regdate;
+				article.memberid = memberid;
+				article.boardid = boardid;
+				articles.add(article);
+			}
 			
 			
 
